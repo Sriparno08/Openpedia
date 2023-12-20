51ccd6167ab6eb7af1b7d1d5684b2f3cdb8edf0e
Sriparno Roy <sriparnoroy01@gmail.com>
2023-12-20 13:45:57 +0530

    Setup 'Openpedia' as a Node app

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, './client/app.js'),
+    output: {
+        path: path.resolve(__dirname, './client'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        static: path.resolve(__dirname, './client'),
+    },
+};
