ab9f1fbece55df3c16123cce94c9744e5b93782b
Neeraj <neerajantil.antil26@gmail.com>
2024-01-18 18:41:42 +0530

    [ISSUE-146] Modify NavBar: Complete, Responsive, and Themed
    This commit addresses issue #146 by implementing modifications to the NavBar. The changes ensure that the NavBar is now complete, responsive, and follows a cohesive theme, enhancing the overall user experience.

--- client/app.js
+++ client/app.js
@@ -1,6 +1,69 @@
-let hamburger = document.querySelector('.hamburger');
-let links = document.querySelector('.links');
+const hamburger = document.querySelector(".hamburger");
+const links = document.querySelector(".links");
+let show = false;
 
-hamburger.addEventListener('click', () => {
-    links.classList.toggle('toggle-links');
-})
+hamburger.addEventListener("click", () => {
+  if (!show) {
+    links.style.transform = 'scale(1)';
+  } else {
+    links.style.transform = 'scale(0)';
+  }
+  show = !show;
+});
+
+// Navbar Modern Design
+
+const styles = {
+  0: {
+    "nav hr" : {
+      borderColor: "rgba(18,38,64)",
+      height: "1px",
+      display: "block",
+      left: "0",
+      right: "0",
+      position: "absolute",
+    },
+    header: {
+      backgroundColor: "rgba(255,255,255,0.5)",
+      boxShadow: "none",
+    },
+    ".title-container .title .link": {
+      color: "#000",
+    },
+    ".item .link": {
+      color: window.innerWidth < 768 ? '#fff' : "#000",
+    },
+  },
+  "non-0": {
+    "nav": {
+      backdropFilter: "blur(10px)",
+    },
+    header: {
+      boxShadow: "0 5px 6px rgba(0,0,0,0.3)",
+    },
+    ".title-container .title .link": {
+      color: "#000",
+    },
+    ".item .link": {
+      color: window.innerWidth < 768 ? '#fff' : "#000",
+    }
+  },
+};
+
+const configNavbar = () => {
+  let scrollPosition = document.documentElement.scrollTop;
+  for (selector in styles[!scrollPosition ? "0" : "non-0"]){
+    let elements = document.querySelectorAll(selector);
+    for (element of elements){
+      for (property in styles[!scrollPosition ? "0" : "non-0"][selector]){
+        element.style[property] = styles[!scrollPosition ? "0" : "non-0"][selector][property];
+      }
+    }
+  }
+  document.querySelector("nav hr").style.width = `${scrollPosition / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100}%`;
+};
+
+// to initialize the styles
+configNavbar();
+
+document.addEventListener("scroll", configNavbar);
