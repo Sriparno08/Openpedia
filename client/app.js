452fe88bd6893095fa62cabf561592c9daf1d7ef
Naveen Chandra Adhikari <nitinadhikari890@gmail.com>
2024-01-20 04:02:13 +0530

    Added button

--- client/app.js
+++ client/app.js
@@ -1,6 +1,16 @@
-let hamburger = document.querySelector('.hamburger');
-let links = document.querySelector('.links');
+let hamburger = document.querySelector(".hamburger");
+let links = document.querySelector(".links");
 
-hamburger.addEventListener('click', () => {
-    links.classList.toggle('toggle-links');
-})
+hamburger.addEventListener("click", () => {
+  links.classList.toggle("toggle-links");
+});
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
