2af26f5628db4fef7a8675e672d0c90c55e0a7ea
mrVioz <jan.weis1997@gmail.com>
2024-05-07 20:29:10 +0200

    updated the anker with a new Hyper-Reference old: ./opensource_programs/index.html new: ./pages/open-source-programs/index.html

--- client/components/footer.js
+++ client/components/footer.js
@@ -27,7 +27,7 @@ footer.innerHTML = `<div class="contain">
                           <p><a href="./git/index.html" class="footer-links">Learn Git & GitHub</a></p>
                           <p><a href="./pages/contributing-to-open-source/index.html" class="footer-links">Contributing to Open Source</a></p>
                           <p><a href="./beginner-friendly-repo/index.html" class="footer-links">Beginner-Friendly Repos</a></p>
-                          <p><a href="./opensource_programs/index.html" class="footer-links">Open Source Programs</a></p>
+                          <p><a href="./pages/open-source-programs/index.html" class="footer-links">Open Source Programs</a></p>
                         </div>
                       </div>
 
