88da33976f32a0ca6e0866373bec2354339cae32
fellalli <fellalli@web.de>
2024-06-21 09:15:11 +0200

    Update header.js

--- client/components/header.js
+++ client/components/header.js
@@ -6,7 +6,7 @@ header.innerHTML = `<div class="title-container">
                         <img src="https://raw.githubusercontent.com/Sriparno08/Openpedia/561ac69fe51a0c328029ae25ec0a13b43386fcbe/client/assets/long-logo.svg" alt="Openpedia">
                       </a>
                     </div>
-                    
+
                     <div class="elemContainer">
                       <div id="themeBody">
                         <button id="themeToggle">
