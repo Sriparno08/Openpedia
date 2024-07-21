e977dc61c0ab18ce193cd1103068ca21c020a5b6
Sriparno Roy <sriparnoroy01@gmail.com>
2024-07-21 13:00:53 +0530

    [UI] Improve Landing Page Footer

--- client/components/footer.js
+++ client/components/footer.js
@@ -7,31 +7,35 @@ footer.innerHTML = `<div class="contain">
                         </div>
 
                         <div class="col">
-                          <h3 id="Support" style="font-size: 15px;">Support</h3>
-                          <p><a href="https://github.com/Sriparno08/Openpedia" class="footer-links">Documentation</a></p>
-                          <p><a href="#about" class="footer-links">About</a></p>
-                          <p style="font-size: 14px; margin-top: 0.5rem;">
-                            <a href="./pages/frequently-asked-questions/index.html" class="hover-link">Frequently Asked Questions</a>
-                          </p>
-                        </div>
-
-                        <div class="col">
                           <h3 id="Solutions" style="font-size: 15px;">Categories</h3>
                           <p><a href="./pages/introduction-to-open-source/index.html" class="footer-links">Introduction to Open Source</a></p>
                           <p><a href="./pages/learn-git-and-github/index.html" class="footer-links">Learn Git & GitHub</a></p>
                           <p><a href="./pages/contributing-to-open-source/index.html" class="footer-links">Contributing to Open Source</a></p>
                           <p><a href="./pages/beginner-friendly-repos/index.html" class="footer-links">Beginner-Friendly Repos</a></p>
                           <p><a href="./pages/open-source-programs/index.html" class="footer-links">Open Source Programs</a></p>
+                        </div>  
+                      
+                        <div class="col">
+                          <h3 id="Support" style="font-size: 15px;">Support</h3>
+                          <p><a href="#about" class="footer-links">About</a></p>
+                          <p><a href="https://github.com/Sriparno08/Openpedia" class="footer-links">Documentation</a></p>
+                          <p><a href="./pages/frequently-asked-questions/index.html" class="footer-links">Frequently Asked Questions</a>
+                          </p>
+                        </div>
+
+                        <div class="col">
+                          <h3>Socials</h3>
+                          <p><a href="https://www.linkedin.com/in/sriparnoroy/" class="footer-links">LinkedIn</a></p>
+                          <p><a href="https://github.com/Sriparno08" class="footer-links">GitHub</a></p>
+                          <p><a href="https://codepen.io/Sriparno08" class="footer-links">CodePen</a></p>
+                          <p><a href="https://dev.to/sriparno08" class="footer-links">DEV</a></p>
+                          <p><a href="https://www.showwcase.com/sriparno08" class="footer-links">Showwcase</a></p>
                         </div>
                       </div>
 
                       <div class="end">
                         <span class="copyright">&copy; 2024 Openpedia</span>
                         <span class="creator">Developed By Sriparno Roy & Open Source Community</span>
-                        <span class="social media" id="socialicons">
-                          <i><a href="https://www.linkedin.com/in/sriparnoroy" class="fa-brands fa-linkedin-in"></a></i>
-                          <i><a href="https://github.com/Sriparno08" class="fa-brands fa-github"></a></i>
-                        </span>
                       </div>
                     </div>`;
 
