7d2991045fbec4dccb1ffee2d12335f58ac150b9
r.navaneeth992 <r.navaneeth992@gmail.com>
2024-08-15 11:55:09 +0530

    Update app.js

--- client/pages/open-source-programs/app.js
+++ client/pages/open-source-programs/app.js
@@ -131,4 +131,35 @@ document.addEventListener("DOMContentLoaded", function () {
     UniversityOpenSourceProgram.forEach(value => {
         dynamicUOSPContainer.innerHTML += createGitItem(value);
     });
+
+    const body = document.body;
+    const themeToggle = document.getElementById("themeToggle");
+
+    themeToggle.addEventListener("click", () => {
+      body.classList.toggle("dark-theme");
+      console.log('themeToggle clicked')
+      // Toggle Font Awesome icons
+
+      const moonIcon = themeToggle.querySelector(".fa-moon");
+      const sunIcon = themeToggle.querySelector(".fa-sun");
+
+      if (body.classList.contains("dark-theme")) {
+        moonIcon.style.display = "inline";
+        sunIcon.style.display = "none";
+        localStorage.setItem("theme", "dark");
+      } else {
+        moonIcon.style.display = "none";
+        sunIcon.style.display = "inline";
+        localStorage.setItem("theme", "light");
+      }
+
+    });
+
+    const savedTheme = localStorage.getItem("theme");
+
+    if (savedTheme === "dark") {
+      body.classList.add("dark-theme");
+    } else {
+      body.classList.remove("dark-theme");
+    }
 });  
